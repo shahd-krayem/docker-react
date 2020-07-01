581b5769e6d0671f478f08a43738f63c615ab3a5
root <root@ip-172-31-46-216.us-west-2.compute.internal>
2020-07-01 08:06:41 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Test Development Env !!
+	  I Was Changed !!
         </p>
         <a
           className="App-link"
